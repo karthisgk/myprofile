f889dccbdce3d68a79bbefe77d00cdfe2773a7b6
karthisgk <karthisg.sg@gmail.com>
2019-06-30 10:40:34 +0530

    sadfdsf

--- src/routes/index.js
+++ src/routes/index.js
@@ -49,6 +49,10 @@ function Routes(app){
 		this.smtp = smtp;
 	});
 
+	app.get('/', function(req, res){
+		res.render('index', {});
+	});
+
 	app.post('/contactme', function(req, res) {
 		res.header('Access-Control-Allow-Origin', '*');
 	    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
